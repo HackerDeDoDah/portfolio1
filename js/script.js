112edaf0df063e41c29b885a023b0bcf7c0f7872
Chris Day <chrisdaypro@protonmail.com>
2024-11-28 13:45:34 +0000

    slide out side menu on smaller screens

--- js/script.js
+++ js/script.js
@@ -1,21 +1,18 @@
 
 
 
-$(document).ready(() => {
-    const $hamburger = $('#hamburger');
-    const $dropdown = $('#dropdown');
-
-    $hamburger.click(() => {
-        $dropdown.toggle();
-    });
-
-    $(document).click((event) => {
-        if (!$dropdown.is(event.target) && !$dropdown.has(event.target).length && !$hamburger.is(event.target)) {
-            $dropdown.hide();
-        }
-    });
+var slideout = new Slideout({
+    'panel': document.getElementById('panel'),
+    'menu': document.getElementById('menu'),
+    'padding': 256,
+    'tolerance': 70,
+    'easing': 'cubic-bezier(.32,2,.55,.27)'
 });
 
+// Toggle button
+document.querySelector('.toggle-button').addEventListener('click', function() {
+    slideout.toggle();
+});
 
 
 document.addEventListener('DOMContentLoaded', () => {
